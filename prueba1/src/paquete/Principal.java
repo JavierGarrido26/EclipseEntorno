df7e2b298840237ae274778dde060ee20fc1d58d
JavierGarrido26 <dam_javiergarrido@riberadeltajo.es>
2023-02-14 16:03:59 +0100

    Otro cambio para el proyecto

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("El cambio a la clase Principal");
+		System.out.println("Otro cambio");
 		
 		
 		
